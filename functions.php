357b9d0c6708d8d40916d4ff01371c7ce4753ca0
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-08-02 22:59:26 +0900

    change functions.php

--- functions.php
+++ functions.php
@@ -5,13 +5,11 @@
 CSSファイルの読み込み
  **************************************************/
 
-function my_enqueue_styles()
-{
-   wp_enqueue_style('ress', '//unpkg.com/ress/dist/ress.min.css', array(), false, 'all');
+function my_enqueue_styles() {
+    wp_enqueue_style('ress', '//unpkg.com/ress/dist/ress.min.css', array(), false, 'all');
     wp_enqueue_style('style', get_stylesheet_uri(), array('ress'), false, 'all');
-}
-add_action('wp_enqueue_scripts', 'my_enqueue_styles');
-
+  }
+  add_action('wp_enqueue_scripts', 'my_enqueue_styles');
 
 
 
