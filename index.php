a5b081acd931588fc35cfddb61255564ec7c90b1
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-08-03 23:15:09 +0900

    delete loop

--- index.php
+++ index.php
@@ -20,12 +20,7 @@
         <!-- workSec -->
         <section class="workSec section wrapper">
 
-        <?php if(have_posts()): ?>
-            <?php while(have_posts()):the_post(); ?>
-                <?php
-                $cat = get_the_category();
-                $catname = $cat[0]->cat_name;
-            ?>
+ 
             <h2 class="section__title">works</h2>
             <ul class="workSec__list">
                 <li class="workSec__item"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></li>
@@ -79,8 +74,6 @@
 
         <div id="pagetop"><a href="#"></a></div>
 
-        <?php endwhile; ?>
-    <?php endif; ?>
     </main>
     <!-- //main -->
 
