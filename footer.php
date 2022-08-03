d82268b80bc47274903fd82a3e153283797d2aa7
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-08-03 22:15:43 +0900

    change footer

--- footer.php
+++ footer.php
@@ -3,13 +3,11 @@
         <p class="small">&copy; 2022 Rika Nakamura</p>
     </footer>
 
-=======
 
     <?php wp_footer(); ?>
 
     <!-- //footer -->
 
->>>>>>> 604fe55ee845f66c13efa8413683d9b2932fcbae
 
 </body>
 
