2eef9db5b7b8b156f9a1061e16a713933d0af816
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-08-03 21:29:46 +0900

    change js

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ function st_enqueue_scripts()
 {
     wp_deregister_script('jquery');
     wp_enqueue_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', array(), '3.5.1', false);
-    wp_enqueue_script('main', get_theme_file_uri('js/main.js'), array('jquery'), false, true);
-    wp_enqueue_script('works-item', get_theme_file_uri('js/works-item.js'), array('jquery'), false, true);
+    wp_enqueue_script('main', get_template_directory_uri('/js/main.js'), array('jquery'), false, true);
+    wp_enqueue_script('works-item', get_template_directory_uri('/js/works-item.js'), array('jquery'), false, true);
 }
 add_action('wp_enqueue_scripts', 'st_enqueue_scripts');
