6b0955edb1fbe0b411667e545c598a8a5ce8197e
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-08-04 22:57:01 +0900

    add single.php

--- single.php
+++ single.php
@@ -5,49 +5,54 @@
 
     <!-- workSec -->
     <section class="worksSec adjustment-top wrapper">
-        <h2 class="section__title">works</h2>
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <h2 class="section__title"><?php the_title(); ?></h2>
 
-        <!-- スライドショー -->
-        <div class="worksSec__slideshow">
-            <div class="slide">
-                <img id="slide_img" src="img/works/works-item2-1.png">
-            </div>
-            <div class="toolbar">
-                <div id="prev" class="button">
-                    < </div>
-                        <div id="page"></div>
-                        <div id="next" class="button">></div>
+                <!-- スライドショー -->
+                <div class="worksSec__slideshow">
+                    <div class="slide">
+                        <img id="slide_img" src="<?php the_post_thumbnail_url('full'); ?>" alt="">
+                    </div>
+                    <div class="toolbar">
+                        <div id="prev" class="button">
+                            < </div>
+                                <div id="page"></div>
+                                <div id="next" class="button">></div>
+                        </div>
+                    </div>
+                    <!-- //スライドショー -->
+
+                    <a href=""></a>
+
                 </div>
-            </div>
-            <!-- //スライドショー -->
+                <p class="worksSec__lead">ポートフォリオサイト(このサイトです)</p>
+                <dl class="worksSec__list">
+                    <dt class="worksSec__dt">URL</dt>
+                    <dd class="worksSec__list-dd"><a href="https://rika-nakamura.github.io/digsmile/" class="link">https://rika-nakamura.github.io/digsmile/</a></dd>
+                    <dt class="worksSec__list-dt">主な特徴</dt>
+                    <dd class="worksSec__list-dd">複数ページ型 /ハンバーガーメニュー / スライドショー / アコーディオン / フェードイン / スムースクロール</dd>
+                    <dt class="worksSec__list-dt">制作範囲</dt>
+                    <dd class="worksSec__list-dd">デザイン・コーディング・スマホ対応・WordPress対応・ロゴやアイコン作成</dd>
+                    <dt class="worksSec__list-dt">使用言語/ツール</dt>
+                    <dd class="worksSec__list-dd">HTML / CSS / JavaScript / jQuery / Figma </dd>
+                    <dt class="worksSec__list-dt">制作期間</dt>
+                    <dd class="worksSec__list-dd">3週間</dd>
+                    <dt class="worksSec__list-dt">作成のポイント</dt>
+                    <dd class="worksSec__list-dd">
+                        <ul>
+                            <li>・PCとスマートフォンで閲覧できるようにレスポンシブ化をしています。</li>
+                            <li>・親しみやすさと、見やすいサイトをコンセプトにデザインを作成しました。自身のキャラクターや親しみやすさを表現するために、オレンジをベースとした配色
+                                と丸みをおびたデザインを意識しました。
+                            </li>
+                            <li>・SassとCSS設計を取り入れることで、管理・更新しやすいサイトを作成しました。</li>
+                        </ul>
+                    </dd>
+                </dl>
+                <div class="btn worksSec__btn"><a href="<?php echo esc_url(home_url('/category/works/')); ?>">戻る</a></div>
 
-            <a href=""></a>
-
-        </div>
-        <p class="worksSec__lead">ポートフォリオサイト(このサイトです)</p>
-        <dl class="worksSec__list">
-            <dt class="worksSec__dt">URL</dt>
-            <dd class="worksSec__list-dd"><a href="https://rika-nakamura.github.io/digsmile/" class="link">https://rika-nakamura.github.io/digsmile/</a></dd>
-            <dt class="worksSec__list-dt">主な特徴</dt>
-            <dd class="worksSec__list-dd">複数ページ型 /ハンバーガーメニュー / スライドショー / アコーディオン / フェードイン / スムースクロール</dd>
-            <dt class="worksSec__list-dt">制作範囲</dt>
-            <dd class="worksSec__list-dd">デザイン・コーディング・スマホ対応・WordPress対応・ロゴやアイコン作成</dd>
-            <dt class="worksSec__list-dt">使用言語/ツール</dt>
-            <dd class="worksSec__list-dd">HTML / CSS / JavaScript / jQuery / Figma </dd>
-            <dt class="worksSec__list-dt">制作期間</dt>
-            <dd class="worksSec__list-dd">3週間</dd>
-            <dt class="worksSec__list-dt">作成のポイント</dt>
-            <dd class="worksSec__list-dd">
-                <ul>
-                    <li>・PCとスマートフォンで閲覧できるようにレスポンシブ化をしています。</li>
-                    <li>・親しみやすさと、見やすいサイトをコンセプトにデザインを作成しました。自身のキャラクターや親しみやすさを表現するために、オレンジをベースとした配色
-                        と丸みをおびたデザインを意識しました。
-                    </li>
-                    <li>・SassとCSS設計を取り入れることで、管理・更新しやすいサイトを作成しました。</li>
-                </ul>
-            </dd>
-        </dl>
-        <div class="btn worksSec__btn"><a href="works.html">戻る</a></div>
+            <?php endwhile; ?>
+        <?php endif; ?>
     </section>
     <!-- //workSec -->
 
