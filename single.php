888e426eb8a5d4e916fb56d7272fcfabeca61924
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-08-04 17:33:19 +0900

    add code single.php

---
+++ single.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="webコーダーをめざす中村理香のポートフォリオサイトです">
+    <title>Rika Nakamura portforio | Works-item1 </title>
+    <link rel="short-icon" href="#">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <link rel="stylesheet" href="styles/reset.css">
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+
+<body>
+    <!-- header -->
+    <header class="header">
+        <div class="header__inner wrapper">
+            <p class="header__logo">
+                <a href="index.html">
+                    <img src="img/Home/logo.png" alt="ヘッダーロゴ">
+                </a>
+            </p>
+            <div class="nav-wrapper">
+                <nav class="header__nav">
+                    <ul class="header__nav_list ">
+                        <li class="header__nav_item"><a href="works.html">works</a></li>
+                        <li class="header__nav_item"><a href="about.html">about</a></li>
+                        <li class="header__nav_item"><a href="contact.html">contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <button class="burger-btn">
+            <span class="bar bar_top"></span>
+            <span class="bar bar_mid"></span>
+            <span class="bar bar_bottom"></span>
+        </button>
+
+    </header>
+    <!-- //header -->
+
+
+    <!-- main -->
+    <main>
+
+        <!-- workSec -->
+        <section class="worksSec adjustment-top wrapper">
+            <h2 class="section__title">works</h2>
+
+            <!-- スライドショー -->
+            <div class="worksSec__slideshow">
+                <div class="slide">
+                    <img id="slide_img" src="img/works/works-item2-1.png">
+                </div>
+                <div class="toolbar">
+                    <div id="prev" class="button">
+                        < </div>
+                            <div id="page"></div>
+                            <div id="next" class="button">></div>
+                    </div>
+                </div>
+                <!-- //スライドショー -->
+
+                <a href=""></a>
+
+            </div>
+            <p class="worksSec__lead">ポートフォリオサイト(このサイトです)</p>
+            <dl class="worksSec__list">
+                <dt class="worksSec__dt">URL</dt>
+                <dd class="worksSec__list-dd"><a href="https://rika-nakamura.github.io/digsmile/" class="link">https://rika-nakamura.github.io/digsmile/</a></dd>
+                <dt class="worksSec__list-dt">主な特徴</dt>
+                <dd class="worksSec__list-dd">複数ページ型 /ハンバーガーメニュー / スライドショー / アコーディオン / フェードイン / スムースクロール</dd>
+                <dt class="worksSec__list-dt">制作範囲</dt>
+                <dd class="worksSec__list-dd">デザイン・コーディング・スマホ対応・WordPress対応・ロゴやアイコン作成</dd>
+                <dt class="worksSec__list-dt">使用言語/ツール</dt>
+                <dd class="worksSec__list-dd">HTML / CSS / JavaScript / jQuery / Figma </dd>
+                <dt class="worksSec__list-dt">制作期間</dt>
+                <dd class="worksSec__list-dd">3週間</dd>
+                <dt class="worksSec__list-dt">作成のポイント</dt>
+                <dd class="worksSec__list-dd">
+                    <ul>
+                        <li>・PCとスマートフォンで閲覧できるようにレスポンシブ化をしています。</li>
+                        <li>・親しみやすさと、見やすいサイトをコンセプトにデザインを作成しました。自身のキャラクターや親しみやすさを表現するために、オレンジをベースとした配色
+                            と丸みをおびたデザインを意識しました。
+                        </li>
+                        <li>・SassとCSS設計を取り入れることで、管理・更新しやすいサイトを作成しました。</li>
+                    </ul>
+                </dd>
+            </dl>
+            <div class="btn worksSec__btn"><a href="works.html">戻る</a></div>
+        </section>
+        <!-- //workSec -->
+
+
+            <!-- cta -->
+            <section class="ctaSec section-underlayer bg-bro">
+                <div class="wrapper">
+                    <h2 class="section__title">contact</h2>
+                    <p>ポートフォリオWebサイトをご覧頂き<br class="only-SP">ありがとうございます。</p>
+                    <p>ご連絡の際は、お気軽に<br class="only-SP">下記リンク先フォームをご利用ください。</p>
+                    <div class="btn"><a href="contact.html">お問い合わせ</a></div>
+                </div>
+            </section>
+            <!-- //cta -->
+
+            <div id="pagetop"><a href="#"></a></div>
+
+    </main>
+    <!-- //main -->
+
+    <!-- footer -->
+    <footer>
+        <small>&copy;2022 Rika Nakamura</small>
+    </footer>
+    <!-- //footer -->
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="js/works-item.js"></script>
+
+</body>
+
+</html>
