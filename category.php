653697d9f29e4ba5be4f4f64d25831efc2132e18
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-08-05 15:37:39 +0900

    add header footer

--- category.php
+++ category.php
@@ -1,113 +1,59 @@
-<!DOCTYPE html>
-<html lang="ja">
+<?php get_header(); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="webコーダーをめざす中村理香のポートフォリオサイトです">
-    <title>Rika Nakamura portforio | Works </title>
-    <link rel="short-icon" href="#">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
-    <link rel="stylesheet" href="styles/reset.css">
-    <link rel="stylesheet" href="styles/styles.css">
-</head>
+<!-- main -->
+<main>
 
-<body>
-    <!-- header -->
-    <header class="header">
-        <div class="header__inner wrapper">
-            <p class="header__logo">
-                <a href="index.html">
-                    <img src="img/Home/logo.png" alt="ヘッダーロゴ">
-                </a>
-            </p>
-            <div class="nav-wrapper">
-                <nav class="header__nav">
-                    <ul class="header__nav_list ">
-                        <li class="header__nav_item"><a href="works.html">works</a></li>
-                        <li class="header__nav_item"><a href="about.html">about</a></li>
-                        <li class="header__nav_item"><a href="contact.html">contact</a></li>
-                    </ul>
-                </nav>
-            </div>
+
+    <!-- workSec -->
+    <section class="workSec  adjustment-top">
+        <div class="wrapper">
+            <h2 class="section__title">works</h2>
+            <ul class="workSec__list">
+                <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item1.png" alt="item1の画像"></a>
+                </li>
+                <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item2.png" alt="item2の画像"></a>
+                </li>
+                <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item3.png" alt="item3の画像"></a>
+                </li>
+                <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item4.png" alt="item4の画像"></a>
+                </li>
+                <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item5.png" alt="item5の画像"></a>
+                </li>
+                <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item6.png" alt="item6の画像"></a>
+                </li>
+                <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item7.png" alt="item7の画像"></a>
+                </li>
+                <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item8.png" alt="item8の画像"></a>
+                </li>
+                <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item9.png" alt="item9の画像"></a>
+                </li>
+            </ul>
+
+            <ul class="pagination">
+                <li>1</li>
+                <li>
+                    <a href="">2</a>
+                </li>
+            </ul>
         </div>
-        <button class="burger-btn">
-            <span class="bar bar_top"></span>
-            <span class="bar bar_mid"></span>
-            <span class="bar bar_bottom"></span>
-        </button>
 
-    </header>
-    <!-- //header -->
-
-    <!-- main -->
-    <main>
-
-
-        <!-- workSec -->
-        <section class="workSec  adjustment-top">
+        <!-- cta -->
+        <section class="ctaSec section-underlayer bg-bro">
             <div class="wrapper">
-                <h2 class="section__title">works</h2>
-                <ul class="workSec__list">
-                    <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item1.png" alt="item1の画像"></a>
-                    </li>
-                    <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item2.png" alt="item2の画像"></a>
-                    </li>
-                    <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item3.png" alt="item3の画像"></a>
-                    </li>
-                    <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item4.png" alt="item4の画像"></a>
-                    </li>
-                    <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item5.png" alt="item5の画像"></a>
-                    </li>
-                    <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item6.png" alt="item6の画像"></a>
-                    </li>
-                    <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item7.png" alt="item7の画像"></a>
-                    </li>
-                    <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item8.png" alt="item8の画像"></a>
-                    </li>
-                    <li class="workSec__item"><a href="works-item2.html"><img src="img/Home/works-item9.png" alt="item9の画像"></a>
-                    </li>
-                </ul>
-
-                <ul class="pagination">
-                    <li>1</li>
-                    <li>
-                        <a href="">2</a>
-                    </li>
-                </ul>
+                <h2 class="section__title">contact</h2>
+                <p>ポートフォリオWebサイトをご覧頂き<br class="only-SP">ありがとうございます。</p>
+                <p>ご連絡の際は、お気軽に<br class="only-SP">下記リンク先フォームをご利用ください。</p>
+                <div class="btn"><a href="contact.html">お問い合わせ</a></div>
             </div>
-
-            <!-- cta -->
-            <section class="ctaSec section-underlayer bg-bro">
-                <div class="wrapper">
-                    <h2 class="section__title">contact</h2>
-                    <p>ポートフォリオWebサイトをご覧頂き<br class="only-SP">ありがとうございます。</p>
-                    <p>ご連絡の際は、お気軽に<br class="only-SP">下記リンク先フォームをご利用ください。</p>
-                    <div class="btn"><a href="contact.html">お問い合わせ</a></div>
-                </div>
-            </section>
-            <!-- //cta -->
-
         </section>
-        <!-- //workSec -->
+        <!-- //cta -->
+
+    </section>
+    <!-- //workSec -->
 
 
-        <div id="pagetop"><a href="#"></a></div>
-    </main>
-    <!-- //main -->
+    <div id="pagetop"><a href="#"></a></div>
+</main>
+<!-- //main -->
 
-    <!-- footer -->
-    <footer>
-        <p class="small">&copy; 2022 Rika Nakamura</p>
-    </footer>
-    <!-- //footer -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="js/main.js"></script>
-
-
-</body>
-
-
-</html>
+<?php get_footer(); ?>
