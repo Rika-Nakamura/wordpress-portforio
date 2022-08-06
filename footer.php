b33a7e586355e6cac5c2adf2d7e167ac4c368401
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-08-06 18:23:32 +0900

    delete footer

--- footer.php
+++ footer.php
@@ -10,13 +10,7 @@
 
 
 
-    <?php if (is_single('works')) : ?>
-        function st_enqueue_scripts()
-        {
-        wp_enqueue_script('works-item', get_theme_file_uri('/js/works-item.js'), array('jquery'), false, true);
-        }
-        add_action('wp_enqueue_scripts', 'st_enqueue_scripts');
-    <?php endif; ?>
+
 
 
     </body>
