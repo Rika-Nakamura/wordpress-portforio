b084fca0ba00b57f1d79d41641a116f9f1aa58fd
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-08-07 12:40:56 +0900

    delete works-item js

--- js/works-item.js
+++ js/works-item.js
@@ -2,22 +2,6 @@
 
 {
 
-  /* aboutページアコーディオン */
-
-  document.addEventListener("DOMContentLoaded", () => {
-    const title = document.querySelectorAll('.js-accordion-title');
-
-    for (let i = 0; i < title.length; i++) {
-      let titleEach = title[i];
-      let content = titleEach.nextElementSibling;
-      titleEach.addEventListener('click', () => {
-        titleEach.classList.toggle('is-active');
-        content.classList.toggle('is-open');
-      });
-    }
-
-  });
-
 
   /* worksページタブ機能 */
   const tabs = document.getElementsByClassName('tab');
