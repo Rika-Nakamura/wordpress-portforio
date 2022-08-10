aa1ab1df1d92c794fff8471ff9a03402aa329792
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-08-10 22:58:02 +0900

    change js

--- js/works-item.js
+++ js/works-item.js
@@ -34,77 +34,47 @@
   });
 
 
+   /* スライドショー */
+   
 
-  {
+    $(function() {
+      $('.slider').slick({
+        dots: true,
+        autoplay: false
+      });
+  });
 
-    /* スライドショー */
-  
-    /* 初期設定 */
-    const imgs = ['img/works/works-item2-1.png', 'img/works/works-item2-2.png', 'img/works/works-item2-3.png'];  // 画像の配列
-    let currentImg = 0;  // スライドショーに表示している画像番号
-    pageNow();  // 現在のページ数を表示する
-  
-  
-    /* スライドショーメソッド */
-    function slideShow(num) {
-      if (currentImg + num >= 0 && currentImg + num < imgs.length) {
-        currentImg += num;
-        document.getElementById('slide_img').src = imgs[currentImg];
-      } else if (currentImg + num === -1) { // 先頭の画像表示時にprevボタンを押したとき
-        currentImg = imgs.length - 1;
-        document.getElementById('slide_img').src = imgs[currentImg];
-      } else if (currentImg + num === imgs.length) { // 末尾の画像表示時にnextボタンを押したとき
-        currentImg = 0;
-        document.getElementById('slide_img').src = imgs[currentImg];
-      }
-      pageNow();
-    }
-  
-    /* 現在のページ数表示メソッド */
-    function pageNow() {
-      document.getElementById('page').textContent = `${currentImg + 1} / ${imgs.length}`;
-    }
-  
-    /* prevボタンクリックイベント */
-    document.getElementById('prev').onclick = function () {
-      slideShow(-1);
-    }
-  
-    /* nextボタンクリックイベント */
-    document.getElementById('next').onclick = function () {
-      slideShow(1);
-    }
-  
-  
-  }
-  
+
+
+
+
+
 
 
 
   /* Topへ戻るボタン */
 
-  jQuery(function(){
+  jQuery(function () {
     var pagetop = jQuery('#pagetop');
     // ボタン非表示
     pagetop.hide();
-  
+
     // 100px スクロールしたらボタン表示
     jQuery(window).scroll(function () {
-       if (jQuery(this).scrollTop() > 100) {
-            pagetop.fadeIn();
-       } else {
-            pagetop.fadeOut();
-       }
+      if (jQuery(this).scrollTop() > 100) {
+        pagetop.fadeIn();
+      } else {
+        pagetop.fadeOut();
+      }
     });
     pagetop.click(function () {
       jQuery('body, html').animate({ scrollTop: 0 }, 500);
-       return false;
+      return false;
     });
   });
 
 
 }
- 
 
 
 
@@ -114,8 +84,9 @@
 
 
 
-  
 
- 
+
+
+
 
 
