c321da6d8694571b72c7c91c5c2076226d5a2c35
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-08-24 18:23:33 +0900

    add loop

--- page-contact.php
+++ page-contact.php
@@ -9,8 +9,11 @@
     <section class="contactSec  adjustment-top wrapper">
         <h2 class="section__title">contact</h2>
         <p class="contactSec__lead">ご連絡の際は、<br class="only-SP">こちらのフォームをご利用ください。</p>
-        
-        <?php echo do_shortcode( '[contact-form-7 id="489" title="コンタクトフォーム 1"]' ); ?>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <?php the_content(); ?>
+        <?php endwhile;
+        endif; ?>
+        <?php echo do_shortcode('[contact-form-7 id="489" title="コンタクトフォーム 1"]'); ?>
     </section>
     <!-- //formSec -->
 
