4d9f5c770e5075ff9bcb7edc1674dd9982e307da
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-08-24 22:48:51 +0900

    only loop

--- page-contact.php
+++ page-contact.php
@@ -10,11 +10,14 @@
         <h2 class="section__title">contact</h2>
         <p class="contactSec__lead">ご連絡の際は、<br class="only-SP">こちらのフォームをご利用ください。</p>
 
-        
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <?php the_content(); ?>
+        <?php endwhile;
+        endif; ?>
 
-        <?php echo do_shortcode( '[contact-form-7 id="489" title="コンタクトフォーム 1"]' ); ?>
 
-       
+
+
     </section>
     <!-- //formSec -->
 
