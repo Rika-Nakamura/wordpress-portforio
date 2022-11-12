1174bb33174ab243a1748a318da7857ec748ec0a
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-11-12 17:44:41 +0900

    jsのコード修正

--- footer.php
+++ footer.php
@@ -5,7 +5,8 @@
 
 
     <?php wp_footer(); ?>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script> -->
 
     <!-- //footer -->
 
