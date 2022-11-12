877d58f9c206a9737cfd672580fc3640e5707ca3
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-11-12 17:53:06 +0900

    jsのコードを元に戻す

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 
     <?php wp_footer(); ?>
     <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script> -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
     <!-- //footer -->
 
