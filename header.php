dabebedf9b7e0971b54e74b35a469c1e46c0b81f
RIKANAKAMURA <4506.nakamura@gmail.com>
2022-11-17 18:51:53 +0900

    パスを取り除くコードの追記

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
     <?php if (is_page('contact')) : ?>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <?php endif; ?> 
+    <?php $dirpath = untrailingslashit( $dirpath ); ?>
 
 
 </head>
